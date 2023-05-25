8499987734ec459264cee700f09abee71a334d1f
root <amr.magdy.hassan95@gmail.com>
2023-05-25 15:10:34 -0700

    pall push

--- push.c
+++ push.c
@@ -10,7 +10,7 @@ void push(stack_t **head, unsigned int line_number)
 	stack_t *newNode;
 	char *num;
 
-	num = strtok(NULL, "\n \t\r");
+	num = strtok(NULL, "\n \t\r$");
 	if (num == NULL)
 	{
 		printf("L%u: usage: push integer\n", line_number);
