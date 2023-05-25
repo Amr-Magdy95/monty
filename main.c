27764ec6a19ce355a87c5ab98830aeb85be6ce7b
root <amr.magdy.hassan95@gmail.com>
2023-05-25 15:43:00 -0700

    ok

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	}
 	while ((read = getline(&line, &len, file)) != -1)
 	{
-		cmd = strtok(line, "\n \t\r$");
+		cmd = strtok(line, "\n \t\r");
 		num++;
 
 		if (cmd)
